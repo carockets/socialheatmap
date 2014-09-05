e9b4ab0c7d786101f5e4a4f93907d91b4288a0df
Carola Boettcher <boettcher.carola@gmail.com>
2014-09-06 00:01:21 +0200

    logging login

--- public/js/app.js
+++ public/js/app.js
@@ -7,8 +7,7 @@ var App = function () {
 		//}[window.location.hostname];
 
 		hello.init({ 
-			facebook : '701500879937628',
-			twitter : 'A1W6ADU6674THULpB4IGtA64l'
+			facebook : 701500879937628,
 		},{redirect_uri:'http://socialheatmap.herokuapp.com/'});
 
 		hello.on('auth.login', function(auth){
